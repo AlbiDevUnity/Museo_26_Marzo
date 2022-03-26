c187233a619f722063dd1448efc0ed7be5ba8870
AlbiDevUnity <albi.rava.003@gmail.com>
2022-03-26 23:02:12 +0100

    fatto il grafico e finito il tutto veramente spero

--- Museo/index.php
+++ Museo/index.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 
 <head>
-    <title>Document</title>
+    <title>Home</title>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/museum.png">
 
     <link href="css/homeStyle.css" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 </head>
 
 <body>
@@ -29,9 +35,19 @@
                 <?php
                     DrawTable("SELECT * FROM prenotazioni", "museo");
                 ?>
+
+                <div class="mb-3">
+                    <label for="InputOrario" class="form-label">Orario</label>
+                    <input class="form-control" id="InputOrario">
+                </div>
+
+                <canvas id="myChart" height="400px", width="400px"></canvas>
             </div>
         </div>
     </div>
+
+    <script src="js/flatpickr.js"></script>
+    <script src="js/chart.js"></script>
 </body>
 
 </html>
